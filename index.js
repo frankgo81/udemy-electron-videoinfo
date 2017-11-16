c5703b9bc3f39bd34312aa8d9b5feb7bd50a74f1
frankgo81 <frankgo81@hotmail.com>
2017-11-16 12:58:47 -0800

    starbucks 11-16

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const electron = require("electron");
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, ipcMain} = electron;
+const ffmpeg = require("fluent-ffmpeg");
+let mainWindow;
 
 app.on("ready",()=>{
    //console.log("app is ready");
- const mainWindow=  new BrowserWindow({});
+       mainWindow=  new BrowserWindow({});
        mainWindow.loadURL(`file://${__dirname}/index.html`);
+})
+
+ipcMain.on('video:submit', (event, path)=>{
+
+ console.log(event);
+ console.log(path);
+ ffmpeg.ffprobe(path, (err, metadata)=>{
+    console.log(mainWindow);
+    mainWindow.webContents.send("video:metadata", metadata.format.duration);
+   //console.log("video duration is", metadata.format.duration);
+ })
+
 })
